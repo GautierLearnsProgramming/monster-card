dcae9afd4934683068d4a1359f276eeb0764a723
Gautier <gautier.blandin@gmail.com>
2021-06-26 19:02:47 +0200

    Remove unused vars

--- src/Grid/grid.component.jsx
+++ src/Grid/grid.component.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import styles from './grid.module.scss';
-import { Row, Col, Image } from 'antd';
+import { Row, Col } from 'antd';
 import MonsterCard from "../MonsterCard/monster-card.component";
 import SearchBar from "./SearchBar/search.component";
 
